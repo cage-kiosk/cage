36c9c52d3f67276ee848c87eb034267003cdda73
Alexander Orzechowski <alex@ozal.ski>
2022-12-16 05:44:26 -0500

    Port 0.16 surface header includes

--- view.h
+++ view.h
@@ -5,7 +5,7 @@
 
 #include <stdbool.h>
 #include <wayland-server-core.h>
-#include <wlr/types/wlr_surface.h>
+#include <wlr/types/wlr_compositor.h>
 #include <wlr/types/wlr_xdg_shell.h>
 #include <wlr/util/box.h>
 #if CAGE_HAS_XWAYLAND
