b6f8f925854cd90592b6f8abb4c8393ec51c9571
Supreeeme <yungwallace@live.com>
2024-02-10 00:24:48 -0500

    xwayland: fix double wl_list_remove
    When destroying an xwayland surface, the dissociate and destroy handlers
    are called, but both of these were removing the map and unmap signal
    handlers, causing a segfault when the destroy handler went to remove
    them.
    Fixes #309

--- xwayland.c
+++ xwayland.c
@@ -139,8 +139,6 @@ handle_xwayland_surface_destroy(struct wl_listener *listener, void *data)
 	struct cg_xwayland_view *xwayland_view = wl_container_of(listener, xwayland_view, destroy);
 	struct cg_view *view = &xwayland_view->view;
 
-	wl_list_remove(&xwayland_view->map.link);
-	wl_list_remove(&xwayland_view->unmap.link);
 	wl_list_remove(&xwayland_view->destroy.link);
 	wl_list_remove(&xwayland_view->request_fullscreen.link);
 	xwayland_view->xwayland_surface = NULL;
