b1551b12594aa81143b3933bc7392ceb75305265
Xtr126 <80520774+Xtr126@users.noreply.github.com>
2024-07-11 08:40:58 +0530

    Update togglekey.h

--- togglekey.h
+++ togglekey.h
@@ -1,1 +1,10 @@
+/* 
+Run cage to obtain key codes:
+$ XTMAPPER_WIDTH=100 XTMAPPER_HEIGHT=100 ./build/cage sh
+Output:
+/dev/input/wl_keyboard: EV_KEY KEY_D DOWN
+/dev/input/wl_keyboard: EV_KEY KEY_W UP
+/dev/input/wl_keyboard: EV_KEY KEY_F11 DOWN
+/dev/input/wl_keyboard: EV_KEY KEY_SUPER_L UP
+*/
 #define TOGGLEKEY "KEY_F11"
