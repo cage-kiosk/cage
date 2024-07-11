d15e4ab409cd6b70c500dd3e4a74d7b7a2b69809
Xtr126 <80520774+Xtr126@users.noreply.github.com>
2024-07-11 09:17:32 +0530

    Add cage_xtmapper.sh

---
+++ cage_xtmapper.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+if [ "$(id -u)" != "0" ]; then
+	echo "This script requires root access to access waydroid shell."
+	exit 1
+fi
+
+if [ $# -eq 0 ]; then
+	echo "User not specified."
+	exit 1
+fi
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+    --user)
+        shift
+        user="$1"
+        ;;
+    *)
+	echo "User not specified."
+        exit 1;;
+    esac
+    shift
+done
+
+export XTMAPPER_WIDTH=${XTMAPPER_WIDTH:-1280}
+export XTMAPPER_HEIGHT=${XTMAPPER_HEIGHT:-720}
+
+su "$user" --command "./build/cage waydroid show-full-ui" | (
+	while [[ -z $(waydroid shell getprop sys.boot_completed) ]]; do
+		sleep 1;
+	done;
+	echo 'exec /system/bin/app_process -Djava.library.path=$(echo /data/app/*/xtr.keymapper*/lib/x86_64) -Djava.class.path=$(echo /data/app/*/xtr.keymapper*/base.apk) / xtr.keymapper.server.RemoteServiceShell "$@"' | waydroid shell -- sh -c 'test -f /data/media/0/Android/data/xtr.keymapper/files/xtMapper.sh || exec cat > /data/media/0/Android/data/xtr.keymapper/files/xtMapper.sh'
+	exec waydroid shell -- sh /sdcard/Android/data/xtr.keymapper/files/xtMapper.sh --wayland-client --width=$XTMAPPER_WIDTH --height=$XTMAPPER_HEIGHT
+)
