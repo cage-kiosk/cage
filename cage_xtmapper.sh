862d1fb1d2ef2eb5c22e00c60a358b29053b33e5
Xtr126 <80520774+Xtr126@users.noreply.github.com>
2024-07-11 09:49:19 +0530

    Update cage_xtmapper.sh

--- cage_xtmapper.sh
+++ cage_xtmapper.sh
@@ -15,8 +15,16 @@ while [ $# -gt 0 ]; do
         shift
         user="$1"
         ;;
+    --window-width)
+        shift
+        XTMAPPER_WIDTH="$1"
+        ;;
+    --window-height)
+        shift
+        XTMAPPER_HEIGHT="$1"
+        ;;
     *)
-	echo "User not specified."
+	echo "Invalid argument"
         exit 1;;
     esac
     shift
@@ -29,6 +37,7 @@ su "$user" --command "./build/cage waydroid show-full-ui" | (
 	while [[ -z $(waydroid shell getprop sys.boot_completed) ]]; do
 		sleep 1;
 	done;
-	echo 'exec /system/bin/app_process -Djava.library.path=$(echo /data/app/*/xtr.keymapper*/lib/x86_64) -Djava.class.path=$(echo /data/app/*/xtr.keymapper*/base.apk) / xtr.keymapper.server.RemoteServiceShell "$@"' | waydroid shell -- sh -c 'test -f /data/media/0/Android/data/xtr.keymapper/files/xtMapper.sh || exec cat > /data/media/0/Android/data/xtr.keymapper/files/xtMapper.sh'
+	echo 'exec /system/bin/app_process -Djava.library.path=$(echo /data/app/*/xtr.keymapper*/lib/x86_64) -Djava.class.path=$(echo /data/app/*/xtr.keymapper*/base.apk) / xtr.keymapper.server.RemoteServiceShell "$@"' |\
+ waydroid shell -- sh -c 'test -f /data/media/0/Android/data/xtr.keymapper/files/xtMapper.sh || exec cat > /data/media/0/Android/data/xtr.keymapper/files/xtMapper.sh'
 	exec waydroid shell -- sh /sdcard/Android/data/xtr.keymapper/files/xtMapper.sh --wayland-client --width=$XTMAPPER_WIDTH --height=$XTMAPPER_HEIGHT
 )
